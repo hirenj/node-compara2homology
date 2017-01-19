99ea29078f239b84b07491df63065298a755b31a
Hiren Joshi <hirenj@gmail.com>
2017-01-19 13:17:25 +0100

    Fix for removing comparadb

--- do_sqlite.sh
+++ do_sqlite.sh
@@ -9,5 +9,5 @@ head -10 seq_member.txt > seq_member_short.txt
 head -10 family_member.txt > family_member_short.txt
 sqlite3 compara.db < sqlcommands-test
 sqlite3 compara.db 'select seq_member_id,stable_id from seq_member'
-rm /codebuild/output; compara.db
+rm compara.db
 sqlite3 compara.db < sqlcommands
