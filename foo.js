eb7267e7055dc7b1f974cda316a51a3b8f86bd35
Hiren Joshi <hirenj@gmail.com>
2017-02-05 23:51:17 +0100

    Properly read ids in

--- foo.js
+++ foo.js
@@ -75,29 +75,51 @@ let bytesread = new BytesRead();
 
 // No
 
-gzipped.pipe(bytesread).pipe(new Gunzip()).pipe(tabular(snake)).pipe(new ObjectTransform()).on('data',(dat) => {
-  let ids = dat.map( (obj) => obj.stable_id );
-  let insert_list = [].concat(ids);
+let merge_lists = function(a,b) {
+  if ( a === b ) {
+    return;
+  }
+  id_maps[b].forEach( id => {
+    if (id_maps[a].indexOf(id) < 0) {
+      id_maps[a].push(id);
+    }
+  });
+  id_maps[a].forEach( id => id_maps[id] = id_maps[a] );
+};
 
+let merge_ids = function(ids) {
   ids.forEach( (id) => {
     if ( ! id_maps[id] ) {
       id_maps[id] = [];
-    } else {
-      id_maps[id].forEach( (existing) => {
-        if ( id_maps[existing].indexOf(id) < 0) {
-          id_maps[existing].push(id);
-        }
-      });
     }
-    insert_list.forEach( (a_id) => {
+    ids.forEach( (a_id) => {
       if (id_maps[id].indexOf(a_id) < 0) {
         id_maps[id].push(a_id);
+        if (id_maps[a_id]) {
+          merge_lists(a_id,id);
+        }
       }
-      id_maps[id]
     });
-    console.log(bytesread.bytesread);
   });
+};
+
+let groups = [ ['A','B','D'], ['C','D','E'],['F','G'],['H','A'] ];
+
+groups.forEach( ids => {
+  merge_ids(ids);
+  console.log(id_maps);
+});
+
+let last_offset = 1024*1024;
+
+gzipped.pipe(bytesread).pipe(new Gunzip()).pipe(tabular(snake)).pipe(new ObjectTransform()).on('data',(dat) => {
+  let ids = dat.map( (obj) => obj.stable_id );
+  merge_ids(ids);
+  if ((bytesread.bytesread % (1024*1024)) < last_offset) {
+    console.log(bytesread.bytesread);
+  }
+  last_offset = (bytesread.bytesread % (1024*1024));
 }).on('end', () => {
-  // fs.writeFile('id_maps.json',JSON.stringify(id_maps), () => console.log("Done"));
+  fs.writeFile('id_maps.json',JSON.stringify(id_maps), () => console.log("Done"));
 });
 
