a0af868a7fdbf172dc7e160e6d8bd374123df6cf
Hiren Joshi <hirenj@gmail.com>
2017-02-24 15:05:30 +0100

    More robustness

--- setup_sqlite.sh
+++ setup_sqlite.sh
@@ -15,10 +15,18 @@ fi
 
 head -10 seq_member.txt > seq_member_short.txt
 head -10 family_member.txt > family_member_short.txt
+
 sqlite3 compara.db < sqlcommands-test
 sqlite3 compara.db 'select seq_member_id,stable_id from seq_member'
+
 rm compara.db
+
 sqlite3 compara.db < sqlcommands
+if [ $? -gt 0 ]; then exit 1; fi
+
+rm seq_member.txt
+rm homology_member.txt
+rm family_member.txt
 
 cd $workdir/pan
 
@@ -28,7 +36,15 @@ fi
 
 head -10 seq_member.txt > seq_member_short.txt
 head -10 family_member.txt > family_member_short.txt
+
 sqlite3 compara.db < sqlcommands-test
 sqlite3 compara.db 'select seq_member_id,stable_id from seq_member'
+
 rm compara.db
+
 sqlite3 compara.db < sqlcommands
+if [ $? -gt 0 ]; then exit 1; fi
+
+rm seq_member.txt
+rm homology_member.txt
+rm family_member.txt
