8bfdc9e824e79d68178478b95da4ae3c5814bcaf
Hiren Joshi <hirenj@gmail.com>
2017-02-27 17:20:21 +0100

    Fix for script path in wrong spot

--- setup_sequence_data.sh
+++ setup_sequence_data.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-targetversion=$1
-workdir=$2
+workdir=$1
 
 for tax in 559292 9606 10090 284812 10029 10116 6239 7227 9823; do
 	curl "http://www.uniprot.org/uniprot/?query=organism%3A$tax&format=tab&columns=id,sequence" > $workdir/$tax.seq.tsv;
 	if [ $? -gt 0 ]; then exit 1; fi
 done
+
+cp seqdbcommands $workdir
 
 cd $workdir
 
